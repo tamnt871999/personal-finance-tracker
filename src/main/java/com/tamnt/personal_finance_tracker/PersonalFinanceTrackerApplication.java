4913df9d405a48857c9cc36b537c77e8abfbf3e4
Tam Nguyen <tamnt871999@gmail.com>
2025-11-24 21:05:48 +0700

    Day 1: Completed project setup, working home controller, and initial commit

---
+++ src/main/java/com/tamnt/personal_finance_tracker/PersonalFinanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tamnt.personal_finance_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamnt/personal_finance_tracker/PersonalFinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamnt.personal_finance_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
